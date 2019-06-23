1f2e13507d938b0a08326d0a7e5832794826cef0
David Francos <opensource@davidfrancos.net>
2019-06-23 19:31:04 +0200

    Trying to fix travis build of metis

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -x
-poetry init -q -n --name=base \
-        --description=base \
-        --author=author  \
+
+# Initialize project with default deps.
+poetry init -q -n --name="{{cookiecutter.project_slug}}" \
+        --description="{{cookiecutter.project_summary}}" \
+        --author="{{cookiecutter.author_name}} <{{cookiecutter.author_email}}>"  \
         --dependency=aiohttp  \
         --dependency=toml \
         --dependency=semver \
@@ -22,16 +24,18 @@ poetry init -q -n --name=base \
         --dev-dependency=pytest-cov
 poetry install
 
-mkdir docs;
-poetry run sphinx-quickstart -q \
+# Make docs structure and defaults
+mkdir docs; poetry run sphinx-quickstart -q \
     --makefile \
     --sep \
     -p "{{cookiecutter.project_slug}}"  \
     -a "{{cookiecutter.author_name}} <{{cookiecutter.author_email}}>"  \
     -v 0.0.0 docs
 
+# Start git flow and get it with its first tag to enable direct auto-changelog usage
 git flow init -d
 git tag 0.0.0
 
+# Configure git hooks
 git config core.hooksPath `pwd`/.git_hooks
 git config gitflow.path.hooks `pwd`/.git_hooks
